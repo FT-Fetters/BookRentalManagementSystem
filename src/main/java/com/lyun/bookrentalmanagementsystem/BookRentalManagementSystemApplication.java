678c13fcd1294177a02c2243ac2f33bd95faa0a5
FT-Fetters <1438255191@qq.com>
2021-12-13 08:35:58 +0800

    Initial commit

---
+++ src/main/java/com/lyun/bookrentalmanagementsystem/BookRentalManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.lyun.bookrentalmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookRentalManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookRentalManagementSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lyun/bookrentalmanagementsystem/BookRentalManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lyun.bookrentalmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookRentalManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
